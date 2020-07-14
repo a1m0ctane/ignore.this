6b386ae38e93a586406ae0d19f9d34b09b432649
a1m0ctane <amir.khan.offenbach.main@gmail.com>
2020-07-14 07:30:31 +0100

    defect #6025 - some work

--- src/main/java/com/acme/basic/HelloWorld.java
+++ src/main/java/com/acme/basic/HelloWorld.java
@@ -4,7 +4,6 @@ public class HelloWorld {
 
   void sayHello() {
     System.out.println("Hello World");
-    System.out.println("add one mor line");
     System.out.println("add one more line");
 
   }
@@ -14,8 +13,6 @@ public class HelloWorld {
 
   void notCovered() {
 
-    System.out.println("This method is not covered by unit tests");
-    System.out.println("This method is not covered by unit tests");
     System.out.println("This method is not covered by unit tests");
     System.out.println("This method is not covered by unit tests");
     System.out.println("Notcovered");
